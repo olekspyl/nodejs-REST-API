4275aa40e17b9ac577413f58b862cee758863b5d
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-05-02 23:28:09 +0200

    add token

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,8 @@ const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../utils");
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner });
   res.json(result);
 };
 
@@ -13,7 +14,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
---
+++ middlewars/authenticate.js
@@ -0,0 +1,25 @@
+const { HttpError } = require("../utils/HttpError");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "secret";
+const { User } = require("../models/user");
+
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const [bearer, token] = authorization.split();
+  if (bearer !== "Bearer") {
+    next(HttpError(401));
+  }
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) {
+      next(HttpError(401));
+    }
+    req.user = user;
+    next();
+  } catch {
+    next(HttpError(401));
+  }
+};
+
+module.exports = authenticate;

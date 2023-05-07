2ea52f4a1303971661c8d18b103d9db2857a462b
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-05-08 00:20:14 +0200

    init

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,27 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs/promises");
 const express = require("express");
 const { schemas } = require("../../models/user");
 const { validateBody, authenticate } = require("../../middlewars");
 const router = express.Router();
 const ctrl = require("../../controllers/users");
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+const tempDir = path.join(__dirname, "temp");
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+});
+
+const upload = multer({
+  storage: multerConfig,
+});
+
+router.post(
+  "/register",
+  upload.single("cover"),
+  validateBody(schemas.registerSchema),
+  ctrl.register
+);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrentUser);
 router.post("/logout", authenticate, ctrl.logout);
